1ee4c552c8755a633af08faa66249875c734dd1a
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2026-01-22 08:20:02 +0530

    small changes

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="ffmpeg_tools",
-    version="0.1.0",
+    version="0.1.1",
     description="A Python package for high-quality FFmpeg command generation and video utilities.",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     author="Sudeep Sudhevan",
-    author_email="Sudeepsudhevan66@gmail.com",
-    url="https://github.com/yourusername/ffmpeg_tools",
+    author_email="sudeepsudhevan66@gmail.com",
+    url="https://github.com/sudeepsudhevan/FFmpeg_pip",
     packages=find_packages(),
     install_requires=[
         "yt-dlp",
