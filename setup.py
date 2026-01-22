d63ac8c1810f08a103a5b2d505693a80a01af92b
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2026-01-22 08:42:00 +0530

    No need of dist folder

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ffmpeg_tools",
-    version="0.1.1",
+    version="0.1.2",
     description="A Python package for high-quality FFmpeg command generation and video utilities.",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
